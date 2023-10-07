5c1beb11161dc72beb79d58d1b2469ea4fbdd33b
Sarthak Chatterjee <sarthatc@gmail.com>
2023-10-08 01:48:48 +0530

    Final Changes

--- backend/app.js
+++ backend/app.js
@@ -4,15 +4,14 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
-const path = require("path");
+const path = require("path");   
 
 const errorMiddleware = require("./middleware/error");
 
 //config
 if (process.env.NODE_ENV !== "PRODUCTION") {
-    dotenv.config({ path: "backend/config/config.env" });
+  require("dotenv").config({ path: "backend/config/config.env" });
 }
-
 
 app.use(express.json());
 app.use(cookieParser());
@@ -29,11 +28,11 @@ app.use("/api/v1", community);
 app.use("/api/v1", resource);
 
 // Let Node serve the files for our built React app
-// app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+app.use(express.static(path.resolve(__dirname, "../frontend/build")));
 
-// app.get("/*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname + "/../frontend/build/index.html"));
-// });
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/../frontend/build/index.html"));
+});
 
 // Middleware for errors
 app.use(errorMiddleware);
