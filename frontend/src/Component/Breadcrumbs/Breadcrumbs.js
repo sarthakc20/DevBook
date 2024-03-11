2b17d2d4200e3903352e61a6e023e6bf27663f03
Sarthak Chatterjee <sarthatc@gmail.com>
2024-03-11 18:10:32 +0530

    Breadcrumbs & CSS Fix

--- frontend/src/Component/Breadcrumbs/Breadcrumbs.js
+++ frontend/src/Component/Breadcrumbs/Breadcrumbs.js
@@ -6,17 +6,25 @@ import "./Breadcrumbs.css";
 const Breadcrumbs = () => {
   const { pathname } = useLocation();
   const pathnames = pathname.split("/").filter((a) => a); // filter to get rid of falsy paths
+  let breadcrumbPath = "";
 
-  // Check if path contains "resources" or "account" or if it has only one segment, and don't render breadcrumbs if true
-  if ((pathnames.includes("resources") || pathnames.includes("account")) || pathnames.length === 1) {
+  // Check if pathnames contain "resources", and don't render breadcrumbs if true
+  if (
+    (pathnames.length === 1 && pathnames[0] === "resources") ||
+    pathnames[0] === "account" ||
+    pathnames[0] === "about" ||
+    pathnames[0] === "contact"
+  ) {
     return null;
   }
 
-  let breadcrumbPath = "";
-
   return (
     <div className="breadcrumbs">
-      {pathnames.length > 0 && <Link to="/"><MdHome /></Link>}
+      {pathnames.length > 0 && (
+        <Link to="/">
+          <MdHome />
+        </Link>
+      )}
       {pathnames.map((name, index) => {
         breadcrumbPath += `${name}`;
         const isLastLink = index === pathnames.length - 1;
@@ -24,7 +32,7 @@ const Breadcrumbs = () => {
         return isLastLink ? (
           <span key={breadcrumbPath}> / {name}</span>
         ) : (
-          <span key={breadcrumbPath}> 
+          <span key={breadcrumbPath}>
             / <Link to={breadcrumbPath}>{name} </Link>
           </span>
         );
