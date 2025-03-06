d69929045c013d01b9acd6d773f146b8317a1b5c
Sarthak Chatterjee <sarthatc@gmail.com>
2025-03-07 00:36:14 +0530

    Fix: mobile menu drawer fix in progress

--- frontend/src/Component/Layout/Header/Header.js
+++ frontend/src/Component/Layout/Header/Header.js
@@ -8,246 +8,131 @@ import { useSelector } from "react-redux";
 import Loader from "../../../Loader/Loader";
 
 const Header = () => {
-  const [openMenue, setOpenMenue] = useState(false);
-
+  const [openMenu, setOpenMenu] = useState(false);
   const { loading, user } = useSelector((state) => state.user);
-
-  function handleNavigation() {
-    const select = document.getElementById("navigationSelect");
-    const selectedOption = select.options[select.selectedIndex].value;
-
-    switch (selectedOption) {
-      case "Community":
-        window.location.href = "/community";
-        break;
-      case "Learn":
-        window.location.href = "/learn";
-        break;
-      default:
-      // Handle any other options if needed
-    }
-  }
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu);
+    document.querySelector(".mobile-nav").classList.toggle("menu-open", !openMenu);
+  };
 
   return (
     <>
       {loading ? (
         <Loader />
       ) : (
-        <>
-          <div className={openMenue ? "menuIcon active" : "menuIcon"}>
-            {user && user ? (
-              <nav>
-                <ul className="navbar-list">
-                  <li className="logo">
-                    <NavLink
-                      className="navbar-link"
-                      style={{ textDecoration: "none" }}
-                      to="/"
-                      onClick={() => setOpenMenue(false)}
-                    >
-                      <img className="logo_img" src={logo} alt="logo" />
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      className="navbar-link"
-                      to="/"
-                      exact="true"
-                      onClick={() => setOpenMenue(false)}
-                    >
-                      Home
-                    </NavLink>
-                  </li>
-                  <li>
+        <header className="header">
+          <div className={`menuIcon ${openMenu ? "active" : ""}`}>
+            <nav>
+              <ul className={`navbar-list ${openMenu ? "open" : ""}`}>
+                <li className="logo">
+                  <NavLink
+                    className="navbar-link"
+                    to="/"
+                    onClick={() => setOpenMenu(false)}
+                  >
+                    <img className="logo_img" src={logo} alt="logo" />
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    className="navbar-link"
+                    to="/"
+                    onClick={() => setOpenMenu(false)}
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    className="navbar-link"
+                    to="/community"
+                    onClick={() => setOpenMenu(false)}
+                  >
+                    Community
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    className="navbar-link"
+                    to="/resources"
+                    onClick={() => setOpenMenu(false)}
+                  >
+                    Resources
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    className="navbar-link"
+                    to="/about"
+                    onClick={() => setOpenMenu(false)}
+                  >
+                    About us
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    className="navbar-link"
+                    to="/contact"
+                    onClick={() => setOpenMenu(false)}
+                  >
+                    Contact
+                  </NavLink>
+                </li>
 
-                  {/* <select
-                      id="navigationSelect"
-                      required
-                      onChange={handleNavigation}
-                    >
-                      <option value="" hidden>
-                        Community
-                      </option>
-                      <option value="Community">Community</option>
-                      <option value="Learn">Learn</option>
-                    </select> */}
-
-                    <NavLink
-                      className="navbar-link"
-                      to="/community"
-                      onClick={() => setOpenMenue(false)}
-                    >
-                      Community
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      className="navbar-link"
-                      to="/resources"
-                      onClick={() => setOpenMenue(false)}
-                    >
-                      Resources
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      className="navbar-link"
-                      to="/about"
-                      onClick={() => setOpenMenue(false)}
-                    >
-                      About us
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      className="navbar-link"
-                      to="/contact"
-                      onClick={() => setOpenMenue(false)}
-                    >
-                      Contact
-                    </NavLink>
-                  </li>
-
-                  <div className="sign-div">
-                    <li className="sign-btn">
-                      <NavLink
-                        className="navbar-link"
-                        to="/account"
-                        onClick={() => setOpenMenue(false)}
-                      >
-                        My Profile
-                      </NavLink>
-                    </li>
-
-                    <li>
-                      <NavLink
-                        className="navbar-link signup"
-                        to="/logout"
-                        onClick={() => setOpenMenue(false)}
-                      >
-                        Logout
-                      </NavLink>
-                    </li>
-                  </div>
-                </ul>
-              </nav>
-            ) : (
-              <nav>
-                <ul className="navbar-list">
-                  <li className="logo">
-                    <NavLink
-                      className="navbar-link"
-                      style={{ textDecoration: "none" }}
-                      to="/"
-                      onClick={() => setOpenMenue(false)}
-                    >
-                      <img className="logo_img" src={logo} alt="logo" />
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      className="navbar-link"
-                      to="/"
-                      exact="true"
-                      onClick={() => setOpenMenue(false)}
-                    >
-                      Home
-                    </NavLink>
-                  </li>
-                  <li>
-                    {/* <select
-                      id="navigationSelect"
-                      required
-                      onChange={handleNavigation}
-                    >
-                      <option value="" hidden>
-                        Community
-                      </option>
-                      <option value="Community">Community</option>
-                      <option value="Learn">Learn</option>
-                    </select> */}
-
-                    <NavLink
-                  className="navbar-link"
-                  to="/community"
-                  onClick={() => setOpenMenue(false)}
-                >
-                  Community
-                </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      className="navbar-link"
-                      to="/resources"
-                      onClick={() => setOpenMenue(false)}
-                    >
-                      Resources
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      className="navbar-link"
-                      to="/about"
-                      onClick={() => setOpenMenue(false)}
-                    >
-                      About us
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      className="navbar-link"
-                      to="/contact"
-                      onClick={() => setOpenMenue(false)}
-                    >
-                      Contact
-                    </NavLink>
-                  </li>
-
-                  <div className="sign-div">
-                    <li className="sign-btn">
-                      <NavLink
-                        className="navbar-link signin"
-                        to="/signin"
-                        onClick={() => setOpenMenue(false)}
-                      >
-                        Sign In
-                      </NavLink>
-                    </li>
-
-                    <li>
-                      <NavLink
-                        className="navbar-link signup"
-                        to="/signup"
-                        onClick={() => setOpenMenue(false)}
-                      >
-                        Sign Up
-                      </NavLink>
-                    </li>
-                  </div>
-                </ul>
-              </nav>
-            )}
-
-            <div className="mobile-nav">
-              <BiMenu
-                name="menu-outline"
-                className="mobile-menu"
-                onClick={() => setOpenMenue(true)}
-              />
-              <MdClose
-                name="close-outline"
-                className="mobile-close"
-                onClick={() => setOpenMenue(false)}
-              />
-            </div>
-
-            {/* <div className="mobile-logo">
-        <NavLink to="/" onClick={() => setOpenMenue(false)}>
-          <img src={logo} alt="logo" />
-        </NavLink>
-      </div> */}
+                <div className="sign-div">
+                  {user ? (
+                    <>
+                      <li className="sign-btn">
+                        <NavLink
+                          className="navbar-link"
+                          to="/account"
+                          onClick={() => setOpenMenu(false)}
+                        >
+                          My Profile
+                        </NavLink>
+                      </li>
+                      <li>
+                        <NavLink
+                          className="navbar-link signup"
+                          to="/logout"
+                          onClick={() => setOpenMenu(false)}
+                        >
+                          Logout
+                        </NavLink>
+                      </li>
+                    </>
+                  ) : (
+                    <>
+                      <li className="sign-btn">
+                        <NavLink
+                          className="navbar-link signin"
+                          to="/signin"
+                          onClick={() => setOpenMenu(false)}
+                        >
+                          Sign In
+                        </NavLink>
+                      </li>
+                      <li>
+                        <NavLink
+                          className="navbar-link signup"
+                          to="/signup"
+                          onClick={() => setOpenMenu(false)}
+                        >
+                          Sign Up
+                        </NavLink>
+                      </li>
+                    </>
+                  )}
+                </div>
+              </ul>
+            </nav>
           </div>
-        </>
+
+          <div className="mobile-nav">
+          <MdClose className="mobile-close" onClick={toggleMenu} />
+          <BiMenu className="mobile-menu" onClick={toggleMenu} />
+          </div>
+        </header>
       )}
     </>
   );
